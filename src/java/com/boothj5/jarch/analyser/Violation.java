52653892b889dbc3d47d40606f765817b25c8a13
Chris Hosegood <chris.hosegood@corelogic.co.uk>
2013-04-08 16:28:30 +0100

    Removed commons-lang

--- src/java/com/boothj5/jarch/analyser/Violation.java
+++ src/java/com/boothj5/jarch/analyser/Violation.java
@@ -21,9 +21,6 @@
  */
 package com.boothj5.jarch.analyser;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class Violation {
 
